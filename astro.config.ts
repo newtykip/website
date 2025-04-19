91ee50533b10098dcef727844017b4e0717083e0
newt <hi@newty.dev>
2025-04-19 20:04:56 +0100

    feat: blog content collection

--- astro.config.ts
+++ astro.config.ts
@@ -1,13 +1,19 @@
+import mdx from "@astrojs/mdx";
+import node from "@astrojs/node";
 import svelte from "@astrojs/svelte";
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+const previewBuild = process.env.PREVIEW !== undefined;
+
 // https://astro.build/config
 export default defineConfig({
-    adapter: vercel(),
-    integrations: [svelte()],
+    adapter: previewBuild ? node({ mode: "standalone" }) : vercel(),
+    integrations: [svelte(), mdx()],
     output: "server",
+
+    site: "https://newty.dev",
 
     vite: {
         plugins: [tailwindcss()],
---
+++ src/content.config.ts
@@ -0,0 +1,12 @@
+import { glob } from "astro/loaders";
+import { defineCollection, z } from "astro:content";
+
+const blog = defineCollection({
+    loader: glob({ base: "./blog", pattern: "**/*.mdx" }),
+    schema: z.object({
+        published: z.coerce.date().optional(),
+        title: z.string(),
+    }),
+});
+
+export const collections = { blog };
