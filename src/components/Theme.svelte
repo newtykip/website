245ad0b251722562a3e4edc8c71193e402687ebf
newt <hi@newty.dev>
2025-04-20 00:59:45 +0100

    feat: themes

---
+++ src/components/Theme.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { theme, themes } from "@/stores/theme";
+</script>
+
+{#each themes as name (name)}
+    <button on:click={() => theme.set(name)}>{name}</button>
+{/each}
